eb0bcb8354e2a29a95b1cb2e8e549c60c05f759f
Nikita-Vaitekunas <X00207670@mytudublin.ie>
2025-03-02 12:32:43 +0000

    Home Template + fixes

--- GameStore/urls.py
+++ GameStore/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('pages.urls')),
     path('users/', include('django.contrib.auth.urls')),
     path('', include('users.urls')),
 ]
