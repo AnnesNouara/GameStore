ea70f753f6d585d61ff6a9e6105cc928d3f13cd8
Nikita-Vaitekunas <X00207670@mytudublin.ie>
2025-03-03 13:37:44 +0000

    Curse thee karl for what you have done

--- pages/urls.py
+++ pages/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
-from .views import GameCreateView
+from .views import GameCreateView, HomePageView
 from . import views
 
 app_name = 'pages'
 
 urlpatterns = [
-    path('',views.prod_list, name='home'),
-    path('<uuid:category_id>/', views.prod_list, name = 'products_by_category'),
-    path('<uuid:category_id>/<uuid:product_id>/', views.product_detail, name = 'product_detail'),
-    path('new/', GameCreateView.as_view(), name = 'game_create'),
+    path('',HomePageView.as_view(), name='home'),
 ]
