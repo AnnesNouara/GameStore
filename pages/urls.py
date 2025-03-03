e788e3316c7e7bd401873f50b0b3be6050b2507b
Annes Nouara <X00215986@mytudublin.ie>
2025-03-03 14:26:18 +0000

    user groups + fixes

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import GameCreateView, HomePageView
+from .views import GameCreateView
 from . import views
 
 app_name = 'pages'
 
 urlpatterns = [
-    path('',HomePageView.as_view(), name='home'),
+    path('', views.prod_list, name = 'all_products'),
+    path('<uuid:category_id>/', views.prod_list, name = 'products_by_category'),
+    path('<uuid:category_id>/<uuid:product_id>/', views.product_detail, name = 'product_detail'),
+    path('new/', GameCreateView.as_view(), name = 'game_create')
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,16 +1,9 @@
-from django.views.generic import ListView, TemplateView
 from .models import Category, Product
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.views.generic.edit import CreateView
 
-class HomePageView(TemplateView):
-    template_name = 'home.html'
-
-class GameListView(ListView):
-    model = Product
-    template_name = 'game_list.html'
-    context_object_name = 'all_games_list'
 
 # Create your views here.
 
@@ -21,18 +14,28 @@ class ManagerRequiredMixin(UserPassesTestMixin):
 class GameCreateView(ManagerRequiredMixin, CreateView):
     model = Product
     fields = ('name', 'category', 'developer', 'picture', 'price', 'stock_quantity', 'description')
-    template_name = 'game/new_game.html'
+    template_name = 'shop/new_product.html'
 
-    def prod_list(request, category_id=None):
-        category = None
-        products = Product.objects.filter(available = True)
+def prod_list(request, category_id=None):
+    category = None
+    products = Product.objects.filter(available = True)
         
-        if category_id:
-            category = get_object_or_404(Category, id=category_id)
-            products = Product.objects.filter(category=category, available = True)
+    if category_id:
+        category = get_object_or_404(Category, id=category_id)
+        products = Product.objects.filter(category=category, available = True)
     
-    #paginator to be put in
+    paginator = Paginator(products, 6)
+    try:
+        page = int(request.GET.get('page','1'))
+    except:
+        page = 1
+    try:
+        products = paginator.page(page)
+    except (EmptyPage,InvalidPage):
+        products = paginator.page(paginator.num_pages)
+        
+    return render(request, 'shop/home.html',{'category':category, 'prods':products})
 
 def product_detail(request, category_id, product_id):
     product = get_object_or_404(Product, category_id=category_id, id=product_id)
-    return render(request, 'game/product.html', {'product':product})
+    return render(request, 'shop/home.html', {'product':product})
