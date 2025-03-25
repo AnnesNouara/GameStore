7512469662a522c07f2aaac297c1b4e755e0a295
Nikita-Vaitekunas <X00207670@mytudublin.ie>
2025-03-25 10:21:59 +0000

    Last fix promise

--- GameStore/settings.py
+++ GameStore/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'middleware.no_cache.NoCacheMiddleware',
 ]
 
 ROOT_URLCONF = 'GameStore.urls'
