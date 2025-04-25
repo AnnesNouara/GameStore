acca4ff14a0d581b796574f9611982ca444bbdb8
KarlMcCarthyMoran <karljm05@gmail.com>
2025-04-25 20:44:51 +0100

    Stock Analysis part3

--- manager_dashboard/views.py
+++ manager_dashboard/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.views.generic import UpdateView, View
 from pages.models import Product
+from django.urls import reverse_lazy, reverse
+from .forms import StockUpdateForm
 
 
 class ManagerRequiredMixin(UserPassesTestMixin):
@@ -10,11 +12,13 @@ class ManagerRequiredMixin(UserPassesTestMixin):
 
 class StockEditView(ManagerRequiredMixin, UpdateView):
     model = Product
-    fields = ('stock')
-    template_name = 'manager_dashboard/edit_stock.html'
-
+    fields = ('stock',)
+    template_name= 'edit_stock.html'
     def get_object(self, queryset=None):
         return self.model.objects.get(id=self.kwargs['product_id'])
+    
+    def get_success_url(self):
+        return reverse('manager_dashboard:stock_detail', kwargs={'product_id': self.object.id})
 
 class StockView(ManagerRequiredMixin, View):
     def get(self, request):
@@ -27,3 +31,5 @@ class StockDetail(ManagerRequiredMixin, View):
         product = get_object_or_404(Product, id=product_id)
         return render(request, 'stock_detail.html', {'product' : product})
     
+    def get_object(self, queryset=None):
+        return self.model.objects.get(id=self.kwargs['product_id'])
